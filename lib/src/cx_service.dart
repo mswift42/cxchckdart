bd9e5bbf99343783e9a8129635cb65491f5895d8
mswift42 <martin.haesler@gmail.com>
2018-01-16 15:19:40 +0000

    Add SearchInputComponent

--- lib/src/cx_service.dart
+++ lib/src/cx_service.dart
@@ -1,6 +1,7 @@
 import 'package:angular/angular.dart' show Injectable;
 import 'package:http/http.dart' as http;
 import 'dart:html';
+import 'dart:async';
 
 @Injectable()
 class CxService {
@@ -12,5 +13,8 @@ class CxService {
   String queryUrl() {
     return 'https://uk.webuy.com/search/index.php?stext=${query}section=&rad_which_stock=3&refinebystore=${location}';
   }
+
+
+
 }
 
---
+++ lib/src/search_input/search_input_component.dart
@@ -0,0 +1,14 @@
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+
+@Component(
+  selector: 'search-input',
+    styleUrls: const ['search_input_component.css'],
+  templateUrl: 'search_input_compoent.html',
+  directives: const [
+    materialDirectives
+  ]
+)
+class SearchInputComponent {
+
+}
