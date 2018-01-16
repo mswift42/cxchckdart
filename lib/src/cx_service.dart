44b9b42409f6d04d7f06c6a69a30da08dc539025
mswift42 <martin.haesler@gmail.com>
2018-01-16 14:35:51 +0000

    Agg query url method.

--- lib/src/cx_service.dart
+++ lib/src/cx_service.dart
@@ -1,7 +1,16 @@
 import 'package:angular/angular.dart' show Injectable;
+import 'package:http/http.dart' as http;
+import 'dart:html';
 
 @Injectable()
 class CxService {
+  String query;
+  String location;
 
+  CxService(this.query, this.location);
+
+  String queryUrl() {
+    return 'https://uk.webuy.com/search/index.php?stext=${query}section=&rad_which_stock=3&refinebystore=${location}';
+  }
 }
 
